c179c84539488c907c6837bac264f004a8606906
diasGustaavo <diasgustaavo@icloud.com>
2023-02-16 13:59:22 -0300

    Initial Commit

---
+++ InstaPets/InstaPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaPetsApp.swift
+//  InstaPets
+//
+//  Created by Gustavo Dias on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
