77a88eeff87b29a67b454cab83ef039757831eae
diasGustaavo <diasgustaavo@icloud.com>
2023-02-20 16:06:16 -0300

    add NavigationLink between LoginView & RegistrationView

--- InstaPets/Core/Authentication/Views/LoginView.swift
+++ InstaPets/Core/Authentication/Views/LoginView.swift
@@ -8,73 +8,79 @@
 import SwiftUI
 
 struct LoginView: View {
-    @Environment(\.dismiss) private var dismiss
-    
     @State private var username: String = ""
     @State private var password: String = ""
     
     var body: some View {
-        ZStack {
-            Color(UIColor(Color.theme.backgroundColor))
-                .ignoresSafeArea(.all)
-            
-            VStack(alignment: .center) {
-                Spacer()
-                    .frame(height: 150)
+        NavigationStack {
+            ZStack {
+                Color(UIColor(Color.theme.backgroundColor))
+                    .ignoresSafeArea(.all)
                 
-                // Header
-                Text("InstaPets").font(Font.custom("Pacifico-Regular", size: 50))
-                    .foregroundColor(Color.theme.foregroundColor)
-                
-                Spacer()
-                    .frame(height: 50)
-                
-                Group {
-                    // Email Field
-                    TextField("Username", text: $username)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 18, weight: .regular))
-                        .frame(height: 40)
+                VStack(alignment: .center) {
+                    Spacer()
+                        .frame(height: 150)
                     
-                    // Password
-                    SecureField("Password", text: $password)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 18, weight: .regular))
-                        .frame(height: 40)
-                    
-                    Text("Forgot Password?")
-                        .font(.caption)
+                    // Header
+                    Text("InstaPets").font(Font.custom("Pacifico-Regular", size: 50))
                         .foregroundColor(Color.theme.foregroundColor)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                }
-                .padding(.vertical, 1)
-                .cornerRadius(7)
-                
-                Spacer()
-                
-                Text("Log In 🐢")
-                    .font(.system(size: 26, weight: .semibold))
-                    .frame(maxWidth: Double.infinity)
-                    .frame(height: 50)
-                    .foregroundColor(Color.theme.accentTextColor)
-                    .background(Color.theme.foregroundColor)
+                    
+                    Spacer()
+                        .frame(height: 50)
+                    
+                    Group {
+                        // Email Field
+                        TextField("Username", text: $username)
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 18, weight: .regular))
+                            .frame(height: 40)
+                        
+                        // Password
+                        SecureField("Password", text: $password)
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 18, weight: .regular))
+                            .frame(height: 40)
+                        
+                        Text("Forgot Password?")
+                            .font(.caption)
+                            .foregroundColor(Color.theme.foregroundColor)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .padding(.vertical, 1)
                     .cornerRadius(7)
-                
-                Divider()
-                    .padding(.vertical, 20)
-                
-                HStack {
-                    Text("Don't have an account?")
-                        .font(.system(size: 14, weight: .regular))
-                        .multilineTextAlignment(.center)
                     
-                    Text("Sign Up.")
-                        .font(.system(size: 14, weight: .bold))
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color.theme.foregroundColor)
+                    Spacer()
+                    
+                    Text("Log In 🐢")
+                        .font(.system(size: 26, weight: .semibold))
+                        .frame(maxWidth: Double.infinity)
+                        .frame(height: 50)
+                        .foregroundColor(Color.theme.accentTextColor)
+                        .background(Color.theme.foregroundColor)
+                        .cornerRadius(7)
+                    
+                    Divider()
+                        .padding(.vertical, 20)
+                    
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.system(size: 14, weight: .regular))
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.theme.basicTextColor)
+                            
+                            Text("Sign Up.")
+                                .font(.system(size: 14, weight: .bold))
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.theme.foregroundColor)
+                        }
+                    }
                 }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         }
     }
 }
